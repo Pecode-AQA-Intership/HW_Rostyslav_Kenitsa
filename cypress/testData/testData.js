5dd17ac449a67e83a127536eacbe1f3fbe0f20f4
Kenitsa <roskenitsa@gmail.com>
2022-02-09 21:25:34 +0200

    master clean

--- cypress/testData/testData.js
+++
@@ -1,9 +0,0 @@
-const faker = require('faker');
-export const BASE_URL = 'https://demoqa.com/text-box';
-export const FAKE_DATA = {
-    name: faker.name.findName(),
-    email: faker.internet.email(),
-    currentAddress: faker.address.streetAddress(),
-    permanentAddress: faker.address.secondaryAddress(),
-    image: faker.image.avatar()
-};
